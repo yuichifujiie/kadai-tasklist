cc791516ee1e5063e483f326204b662d33728f15
EC2 Default User <ec2-user@ip-172-31-43-12.us-east-2.compute.internal>
2018-06-08 05:19:56 +0000

    size layout3

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
 <h1>タスク新規作成ページ</h1>
 
     <div class="row">
-        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
 
         {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
@@ -13,7 +13,7 @@
             {!! Form::text('content', null, ['class' => 'form-control']) !!}
         
             {!! Form::label('status', 'ステータス:') !!}
-            {!! Form::select('status',['0%', '50%', '100%'], null, ['class' => 'form-control']) !!}
+            {!! Form::select('status',['0%'=>'0%', '50%'=>'50%', '100%'=>'100%'], null, ['class' => 'form-control']) !!}
         
             {!! Form::submit('投稿',  ['class' => 'btn btn-primary']) !!}
 
